970fafab1ae1f1bf08ad391512cc546a281a9e7b
Morten M. Hansen <mmh@bellcom.dk>
2014-06-06 14:48:04 +0200

    color ctslog

--- jobs/ctslog.rb
+++ jobs/ctslog.rb
@@ -16,14 +16,25 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
   mash = Hashie::Mash.new response
   data = [];
   date_and_time = '%Y-%m-%d %H:%M:%S %Z'
-  
+  color = "blue"
+
   mash.hits.hits.each do |result|
         x = DateTime.strptime(result._source.received_at,date_and_time)
         # quick way to add 2 hours :)
         x = x + Rational(60*60*2, 86400)
+        case result._source.syslog_pri
+        when "130"
+          color = "red"
+        when "132"
+          color = "yellow"
+        when "133"
+          color = "#19FF19"
+        when "134"
+          color = "black"
+        end
         data.push({
           label: x.strftime("%F %T"),
-          value: result._source.syslog_message
+          value: "<span style=color:" + color + ">" + result._source.syslog_message + "</span>"
         });
   end
   
